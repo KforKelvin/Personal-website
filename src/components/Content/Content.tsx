6756086a574035d629981434a1ce43b38ca9c65e
KforKelvin <ky1015@tamu.edu>
2024-01-30 18:15:29 -0600

    updated content

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -26,11 +26,25 @@ export function Content() {
           <p>
             I use my phone and my computer a lot. I like to watch Netflix shows
             (Blue Eye Samurai, One Piece), gaming videos, and cooking videos
-            with my phone.
+            with my phone. I also play some video games. Such as Diablo 4,
+            Overwatch, and the dark souls series.
           </p>
           <p>
-            I also play some video games. Such as Diablo 4, Overwatch, and the
-            dark souls series.
+            I am mainly a consumer of the content. I don't really create
+            contents.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft">
+          <h3>Challenge with the tools: </h3>
+        </ScrollAnimation>
+        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
+          <p>
+            One of the difficulty I encounter when playing videos game or
+            watching a show would be internet stability. I live in a townhouse
+            with three other roommates, and the internet bandwith is not enough
+            for all of us. Sometimes, when I am playing a game, my ping would
+            spike up to 2000ms, and I would get disconnected from the game. It
+            is very frustrating.{" "}
           </p>
         </ScrollAnimation>
       </div>
