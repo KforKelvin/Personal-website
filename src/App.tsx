56815ef90e50403f24a09c27bcda9bf94f350e4a
KforKelvin <ky1015@tamu.edu>
2024-02-20 17:58:20 -0600

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { About } from "./components/About/About";
-import { Hero } from "./components/Hero/Hero";
-import { Main } from "./components/Main/Main";
+// import { Hero } from "./components/Hero/Hero";
+// import { Main } from "./components/Main/Main";
 import { GlobalStyle } from "./styles/global";
 import { Assignment2 } from "./components/Assignment2/Assignment2";
 
